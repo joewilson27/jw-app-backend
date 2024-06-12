f45d7a1648a88d7afbbd5485bffbdc7e067b8559
Joe Wilson <wilsonmangunsong@gmail.com>
2024-06-12 16:29:21 +0700

    first-commit
    start from here

---
+++ src/main/java/jw/apps/backend/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package jw.apps.backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  
+}
