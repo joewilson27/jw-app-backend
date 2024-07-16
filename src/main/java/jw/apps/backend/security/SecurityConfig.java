387207e24e28448930d8de83015ec773bf557172
Joe Wilson <wilsonmangunsong@gmail.com>
2024-07-16 20:42:38 +0700

    add-enablemethodsecurity-on-config
    add-enablemethodsecurity-on-config

--- src/main/java/jw/apps/backend/security/SecurityConfig.java
+++ src/main/java/jw/apps/backend/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,7 +27,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableMethodSecurity 
+@EnableMethodSecurity
+@EnableWebSecurity 
 public class SecurityConfig {
   
   @Autowired
